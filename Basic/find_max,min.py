174af1b492cb4b179a1ad67562d6fe17fa05f94b
HO <godqhr721@gmail.com>
2022-08-06 19:36:24 +0900

    modified if line

--- Basic/find_max,min.py
+++ Basic/find_max,min.py
@@ -1,6 +1,5 @@
 
-
-arr = tuple(map(int,input().split()))
+arr = tuple(map(int ,input().split()))
 
 max_val = -999
 min_val = 999
@@ -9,12 +8,10 @@ for elem in arr:
     if(elem == -999 or elem == 999):
         break
 
-    elif(elem > max_val):
+    if(elem > max_val):
         max_val = elem
-        if(elem < min_val):
-            min_val = elem
-    elif(elem < min_val):
+
+    if(elem < min_val):
         min_val = elem
-        if(elem > max_val):
-            max_val = elem
+
 print(f"{max_val} {min_val}")
